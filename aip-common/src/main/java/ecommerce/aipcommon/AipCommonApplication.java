f732684b5e4ff575c5970e7e87061ea9a67a5fa8
DucNghia176 <nghi7a@gmail.com>
2025-07-08 15:48:07 +0700

    update

---
+++ aip-common/src/main/java/ecommerce/aipcommon/AipCommonApplication.java
@@ -0,0 +1,13 @@
+package ecommerce.aipcommon;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AipCommonApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AipCommonApplication.class, args);
+    }
+
+}
---
+++ aip-common/src/test/java/ecommerce/aipcommon/AipCommonApplicationTests.java
@@ -0,0 +1,13 @@
+package ecommerce.aipcommon;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AipCommonApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
