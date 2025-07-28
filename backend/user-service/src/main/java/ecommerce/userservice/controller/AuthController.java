774474901dcfc5ea43b93385c899b5c53d7c2ce0
DucNghia176 <nghi7a@gmail.com>
2025-07-28 21:41:20 +0700

    frontend user

--- backend/user-service/src/main/java/ecommerce/userservice/controller/AuthController.java
+++ backend/user-service/src/main/java/ecommerce/userservice/controller/AuthController.java
@@ -6,18 +6,16 @@ import ecommerce.userservice.dto.request.AuthRequest;
 import ecommerce.userservice.service.AuthService;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequiredArgsConstructor
+@CrossOrigin(origins = "http://localhost:4200")
 @RequestMapping("/auth")
 public class AuthController {
     private final AuthService authService;
 
-    @GetMapping("/login")
+    @PostMapping("/login")
     public ApiResponse<AuthResponse> login(@Valid @RequestBody AuthRequest request) {
         return authService.login(request);
     }
--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,3 +1,9 @@
-import { Routes } from '@angular/router';
+import {Routes} from '@angular/router';
+import {UserComponent} from "./component/admin/user/user.component";
+import {LoginComponent} from "./component/auth/login/login.component";
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {path: '', redirectTo: 'login', pathMatch: 'full'},
+  {path: 'login', component: LoginComponent},
+  {path: 'users', component: UserComponent},
+];
