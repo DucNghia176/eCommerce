c8c28493233c8c3a8ce000bc07c78d5dafa3a8c6
DucNghia176 <nghi7a@gmail.com>
2025-07-28 21:41:41 +0700

    frontend user

---
+++ frontend/src/app/services/users.service.ts
@@ -0,0 +1,38 @@
+import {Injectable} from '@angular/core';
+import {HttpClient} from '@angular/common/http';
+import {catchError, map, Observable, throwError} from 'rxjs';
+import {UserResponse} from '../models/user.model';
+import {ApiResponse} from "../models/common.model";
+
+@Injectable({
+  providedIn: 'root'
+})
+export class UsersService {
+  private apiUrl = 'http://localhost:8085/users';
+
+  constructor(private http: HttpClient) {
+  }
+
+  getAllUsers(): Observable<UserResponse[]> {
+    return this.http.get<ApiResponse<UserResponse[]>>(`${this.apiUrl}/all`)
+      .pipe(
+        map(response => {
+          if (response.code === 200 && response.data) {
+            return response.data;
+          }
+          throw new Error(response.message || 'Something went wrong');
+        }), catchError(this.handleError)
+      );
+  }
+
+  private handleError(error: any): Observable<never> {
+    let errorMessage = 'Đã xảy ra lỗi';
+    if (error.error instanceof ErrorEvent) {
+      errorMessage = error.error.message;
+    } else {
+      errorMessage = error.error.message || `Mã lỗi: ${error.status}\nThông báo: ${error.message}`;
+    }
+    console.error(errorMessage);
+    return throwError(() => new Error(errorMessage));
+  }
+}
