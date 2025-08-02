9ba17d8a1598a67a45833cc18a0dc5675706dc23
DucNghia176 <nghi7a@gmail.com>
2025-08-02 20:43:30 +0700

    sửa hàm

--- backend/user-service/src/main/java/ecommerce/userservice/service/impl/UserServiceImpl.java
+++ backend/user-service/src/main/java/ecommerce/userservice/service/impl/UserServiceImpl.java
@@ -4,8 +4,6 @@ import ecommerce.aipcommon.config.TokenInfo;
 import ecommerce.aipcommon.model.response.ApiResponse;
 import ecommerce.aipcommon.model.response.UserResponse;
 import ecommerce.aipcommon.model.status.RoleStatus;
-import ecommerce.aipcommon.util.JwtUtil;
-import ecommerce.userservice.dto.request.UserRequest;
 import ecommerce.userservice.dto.request.UserUpdateRequest;
 import ecommerce.userservice.entity.Users;
 import ecommerce.userservice.kafka.KafkaUser;
@@ -15,7 +13,6 @@ import ecommerce.userservice.service.CloudinaryService;
 import ecommerce.userservice.service.UserService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
 
@@ -28,64 +25,9 @@ import java.util.List;
 public class UserServiceImpl implements UserService {
     private final UserRepository userRepository;
     private final UserMapper userMapper;
-    private final PasswordEncoder passwordEncoder;
     private final KafkaUser kafkaUser;
     private final CloudinaryService cloudinaryService;
-    private final JwtUtil jwtUtil;
     private final TokenInfo tokenInfo;
-
-    @Override
-    public ApiResponse<UserResponse> createUser(UserRequest request, MultipartFile avatarFile) {
-        try {
-            if (userRepository.existsByUsername(request.getUsername())) {
-                return ApiResponse.<UserResponse>builder()
-                        .code(400)
-                        .message("Tên đăng nhập đã tồn tại")
-                        .data(null)
-                        .build();
-            }
-
-            if (userRepository.existsByEmail(request.getEmail())) {
-                return ApiResponse.<UserResponse>builder()
-                        .code(400)
-                        .message("Email đã tồn tại")
-                        .data(null)
-                        .build();
-            }
-            Users users = userMapper.toEntity(request);
-
-            users.setRole(RoleStatus.USER.toString());
-            if (avatarFile != null && !avatarFile.isEmpty()) {
-                String avatarUrl = cloudinaryService.uploadFile(avatarFile);
-                users.setAvatar(avatarUrl);
-            }
-            String encodedPassword = passwordEncoder.encode(request.getPassword());
-            users.setPassword(encodedPassword);
-            users.setCreatedAt(LocalDateTime.now());
-            try {
-                users.setGender(request.getGender());
-            } catch (IllegalArgumentException e) {
-                return ApiResponse.<UserResponse>builder()
-                        .code(400)
-                        .message("Giới tính không hợp lệ. Chỉ được phép 'Nam' hoặc 'Nữ'")
-                        .data(null)
-                        .build();
-            }
-            UserResponse response = userMapper.toResponse(userRepository.save(users));
-            return ApiResponse.<UserResponse>builder()
-                    .code(200)
-                    .message("Tạo user thành công")
-                    .data(response)
-                    .build();
-        } catch (Exception e) {
-            log.error("Lỗi: {}", e.getMessage(), e);
-            return ApiResponse.<UserResponse>builder()
-                    .code(500)
-                    .message("Tạo user không thành công")
-                    .data(null)
-                    .build();
-        }
-    }
 
     @Override
     public ApiResponse<UserResponse> getUserById() {
