d3634df5297ca93e5b413ec8d90d92fd325300ed
DucNghia176 <nghi7a@gmail.com>
2025-08-02 19:48:13 +0700

    update url

--- backend/user-service/src/main/java/ecommerce/userservice/controller/AuthController.java
+++ backend/user-service/src/main/java/ecommerce/userservice/controller/AuthController.java
@@ -11,7 +11,7 @@ import org.springframework.web.bind.annotation.*;
 @RestController
 @RequiredArgsConstructor
 @CrossOrigin(origins = "http://localhost:4200")
-@RequestMapping("/auth")
+@RequestMapping("/api/auth")
 public class AuthController {
     private final AuthService authService;
 
