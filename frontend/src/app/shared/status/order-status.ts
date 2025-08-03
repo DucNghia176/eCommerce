2e400a31e2a6449f4456bdede3b8b2890e2e37dd
DucNghia176 <nghi7a@gmail.com>
2025-08-03 22:27:28 +0700

    status

---
+++ frontend/src/app/shared/status/order-status.ts
@@ -0,0 +1,8 @@
+export enum OrderStatus {
+  pending = 'PENDING',
+  confirmed = 'CONFIRMED',
+  shipping = 'SHIPPING',
+  delivered = 'DELIVERED',
+  cancelled = 'CANCELLED',
+  failed = 'FAILED'
+}
