22c55f565d70debd013abbe480d3764a178c71f8
DucNghia176 <nghi7a@gmail.com>
2025-09-11 15:03:27 +0700

    update

---
+++ frontend/src/app/core/models/orders.model.ts
@@ -0,0 +1,14 @@
+import {OrderStatus} from "../../shared/status/order-status";
+import {PaymentStatus} from "../../shared/status/payment-status";
+
+export interface OrderAD {
+  id: number;
+  orderCode: string;
+  orderDate: Date;
+  customerId: number;
+  customerName: string;
+  paymentStatus: PaymentStatus;
+  orderStatus: OrderStatus;
+  orderAmount: string;
+  formattedDate: string;
+}
