64b0ad8823f460df54528b0e0ba59f009dbf54ce
DucNghia176 <nghi7a@gmail.com>
2025-12-08 16:08:47 +0700

    thêm

--- frontend/src/environments/environment.production.ts
+++ frontend/src/environments/environment.production.ts
@@ -1,6 +1,7 @@
 export const environment = {
   production: true,
   apiUrl: 'https://carolyn-unenlarged-marshall.ngrok-free.dev/api/'
+  ,
   provinceBaseUrl: 'https://provinces.open-api.vn/api'
 };
 
